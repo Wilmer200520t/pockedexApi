4fd3a45c4d7727043c419e952b34e59258f0d85b
Wilmer Zuñiga <wilmer.franco.65@gmail.com>
2024-07-28 18:35:37 -0500

    feat: Add http adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly Axios: AxiosInstance = axios;
-
   constructor(
     private readonly PokemonService: PokemonService,
+    private readonly http: AxiosAdapter,
     @InjectModel(Pokemon.name)
     private readonly PokemonModel: Model<Pokemon>,
   ) {}
 
   async runSeed() {
     await this.PokemonModel.deleteMany({});
-    const { data } = await this.Axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 

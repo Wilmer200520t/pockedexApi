50f6a432aa852db3d046fdeddc8e9f560bd7e28b
Wilmer Zuñiga <wilmer.franco.65@gmail.com>
2024-07-29 13:09:02 -0500

    fix: Fix Joi MONGO_URL validation and add Mongo database

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: 'pokemonsdb',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-  MONGO_UR: Joi.required(),
+  MONGO_URL: Joi.required(),
   APP_PORT: Joi.number().default(3000),
   DEFAULT_LIMIT: Joi.number().default(10),
 });
